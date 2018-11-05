751ee434e66704b4f3e54970b7f0829d982734a4
Ranjith Cheguri <theeverindian@gmail.com>
2018-11-04 23:20:00 -0800

    git error resolution

--- lab2/HomeAway/kafka/services/books.js
+++ lab2/HomeAway/kafka/services/books.js
@@ -2,7 +2,6 @@
   var books = [
     {"BookID" : "1", "Title" : "Book 1", "Author" : "Author 1"},
     {"BookID" : "2", "Title" : "Book 2", "Author" : "Author 2"},
-    {"BookID" : "3", "Title" : "Book 3", "Author" : "Author 3"}
 ]
 
 function handle_request(msg, callback){
